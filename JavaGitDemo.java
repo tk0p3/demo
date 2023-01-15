9c8c68cef986a36fd404e8504909ff9702187771
tk0p3 <105395141+tk0p3@users.noreply.github.com>
2023-01-15 12:57:08 +0100

    added method placeholders

--- JavaGitDemo.java
+++ JavaGitDemo.java
@@ -2,8 +2,25 @@ public static JavaGitDemo {
  
   public static void main(String[] args) {
    
-    
+    doFirst();
+   doSecond();
+   doThird();
+   
     
   }
+ 
+ // placeholders 
+ 
+ private void doFirst() {
+ 
+ }
+ 
+ private void doSecond() {
   
+ }
+  
+ private void doThird() {
+  
+ }
+ 
 }
